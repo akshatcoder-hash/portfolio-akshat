4c23e1217031ba9097816998f13dcd93941f4069
akshatcoder-hash <akshatcoder@outlook.com>
2023-03-09 20:08:24 +0530

    about me

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,16 +1,10 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
-  `,
+  name: "akshat sharma",
+  jobDescription: "Full Stack Web3 Developer",
+  about: `a full-stack web3 developer!`,
+
+  intro: `i drink coffee ☕ and ship some dope shit 🚀.`,
 
   experience: [
     {
